1fba2fd74dafd1aa238ef44425b5a485f5672cf4
MatiusJBG <benjaaigaje01@gmail.com>
2025-12-10 11:43:42 -0500

    ola

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || 'https://proyectomycs2.onrender.com';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 export default API_URL;
